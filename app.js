c7cb53de6755f23af74591030f19bc4bdb02985b
pramith <malagodap2@gmail.com>
2022-12-11 13:58:40 +0530

    Js changes added.

---
+++ app.js
@@ -0,0 +1,8 @@
+// Set initial count
+let count = 0;
+
+// Select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelector('.btns');
+
+console.log(btns);
