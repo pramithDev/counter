7fa91e78972928a6cd7c0aa98f5df8c53b013108
pramith <malagodap2@gmail.com>
2022-12-12 23:30:19 +0530

    counter changes added..

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ let count = 0;
 
 // Select value and buttons
 const value = document.querySelector('#value');
-const btns = document.querySelector('.btns');
+const btns = document.querySelectorAll('.btn');
 
-console.log(btns);
+// console.log(btns);
+
+btns.forEach(function (btn) {
+    //console.log(btn);
+    btn.addEventListener('click', function (e) {
+        //console.log(e.currentTarget.classList);
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }
+        value.textContent = count;
+    });
+})
