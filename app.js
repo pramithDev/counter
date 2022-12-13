3d6479dbf07d3837f9612e491ae8126ecd0d8f69
pramith <malagodap2@gmail.com>
2022-12-13 22:56:34 +0530

    color changes added..

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ btns.forEach(function (btn) {
         }else{
             count = 0;        
         }
+
+        if(count > 0){
+            value.style.color = 'green';
+        }
+        if(count === 0){
+            value.style.color = 'red';
+        }
         value.textContent = count;
     });
 })
